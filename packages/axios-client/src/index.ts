476e427095a8e31eb67d0866d0e7f0c9b27b83d1
Jimmy Thomson <jimmy@gradientedge.com>
2023-02-28 14:41:30 +0000

    Add X-Retry-Count header to retried requests

--- packages/axios-client/src/index.ts
+++ packages/axios-client/src/index.ts
@@ -34,6 +34,10 @@ export function getAxiosClient(options?: RequestConfig) {
         // behaviour doesn't consider timeouts 'retryable'.
         return isRequestRetryable(error, options?.retry?.retryStatusCodes ?? [])
       },
+      onRetry: (retryCount, error, requestConfig) => {
+        requestConfig.headers ??= {}
+        requestConfig.headers['X-Retry-Count'] = retryCount
+      },
     })
   }
 
